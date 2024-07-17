cdf5bab077f7b24ee1f46aec79ffa0a7943c94a4
Treeya14 <triyasaenjan@gmail.com>
2024-07-17 15:10:08 +0700

    first commit

---
+++ src/app/aecranking/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AECRanking from "../components/AECRanking";
+
+export default function page() {
+  return (
+    <div>
+      page
+      <AECRanking />
+    </div>
+  );
+}
